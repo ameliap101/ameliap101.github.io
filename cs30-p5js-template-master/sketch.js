47454398575d07e6f84646860c4c7072e2b6072c
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-02-05 14:13:41 -0600

    demo

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -5,22 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let r = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0);
-  fill(r,0,0);
-  ellipse(mouseX, mouseY, 40, 40);
-}
-function mousePressed(){
-  if (r === 0){
-    r = 255;
-  }
-  else{
-    r = 0;
-  }
+  background(220);
 }
