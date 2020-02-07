cc359340337a3acf9c2a59c69d2eba66a686e8ad
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-02-07 14:12:10 -0600

    scene

--- Interactive scene/sketch.js
+++ Interactive scene/sketch.js
@@ -4,7 +4,7 @@
 //
 // Scene where you can interact with character
 
-
+let currentScene = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 x = width/4;
@@ -13,21 +13,58 @@ y = height/4;
 
 function draw() {
   background(52, 235, 204);
-  grass();
+  scene();
   character();
 }
 
-function grass(){
+function keyPressed(){
+  if (keyIsPressed === true){
+    if (keyCode === LEFT_ARROW){
+      if(currentScene===0) currentScene=1;
+    }
+  else if (keyCode === RIGHT_ARROW){
+    nextScene();
+  }
+  }
+}
+
+
+function scene(){
   fill(38, 110, 41);
   rect(0,(windowHeight/5)*4,windowWidth, windowHeight);
+  strokeWeight(0);
+  fill(115, 69, 46);
+  rect(760,131,100,500);//tree base
+  fill(15, 87, 30);
+  ellipse(780,90,300,310);//tree leaf
+  ellipse(760,160,300,310);//
+  ellipse(860,190,300,310);//
+  ellipse(870,100,300,310);//
+  fill(230, 226, 44);
+  ellipse(100,100,200,200);//sun
+  fill(0);
+  textSize(25);
+  
+  text('Amelia Pilon',50,750);
 }
+
 function character(){
-  fill(110, 60, 38);
-  strokeWeight(1);
-  ellipse(mouseX, mouseY,100, 80);//head
-  fill(255, 255, 255);
-  ellipse(mouseX+20,mouseY-5,30,20);//eye
-  fill(0,0,0);
-  ellipse(mouseX+20,mouseY-5,10,10);//inside eye
-  triangle(mouseX+45,mouseY+30,mouseX-45,mouseY-60,mouseX+45,mouseY-90);
+  strokeWeight(.5);
+  fill(230, 87, 123);
+  ellipse(mouseX-40,mouseY+40,80,100);//left bottom wing
+  fill(173, 87, 230);
+  ellipse(mouseX-40,mouseY-40,90,110);//left top wing
+  fill(230, 208, 87);
+  fill(230, 87, 123);
+  ellipse(mouseX+40,mouseY+40,80,100);//right bottom wing
+  fill(173, 87, 230);
+  ellipse(mouseX+40,mouseY-40,90,110);//right top wing
+  fill(230, 208, 87);
+  ellipse(mouseX, mouseY,40, 130);//body base 
+}
+function nextScene(){
+  background(9, 44, 71);
+  fill(38, 110, 41);
+  rect(0,(windowHeight/5)*4,windowWidth, windowHeight);
+  character();
 }
