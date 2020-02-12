f4a9208a82acd649f9db23c732d3882f7f685ede
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-02-12 14:10:43 -0600

    rectangle

---
+++ 01 Partner State Project/sketch.js
@@ -0,0 +1,32 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x = 0;
+let y = 0;
+let state;
+let SPEED;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(x,y,40,40);
+  moveRectangle();
+}
+
+
+function moveRectangle(){
+  if (state === 0){
+    x+SPEED;
+    
+  }
+  elseif
+  
+
+}
---
+++ rollover project/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(0,0,width/2,height/2); //top left rectangle
+  rect(width/2,0,width/2,height/2); //top right rectangle
+  rect(0,height/2,)
+}
