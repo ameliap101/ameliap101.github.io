9fc815bdf2036ea39ba3836df2a09f1a5c9e5c60
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-02-24 14:10:43 -0600

    rectangle

--- rollover project/sketch.js
+++ rollover project/sketch.js
@@ -1,12 +1,15 @@
-// Project Title
-// Your Name
-// Date
+// Rollover
+// Amelia Pilon
+// Feb. 24th
 //
-// Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let onTopLeft, onTopRight, onBottomLeft,onBottomRight; //boolean state variable
-let leftFade = 0;
-let rightFade = 0;
+
+let q1Fade = 255;
+let q2Fade = 255;
+let q3Fade = 255;
+let q4Fade = 255;
+
 let FADE_SPEED = 2;
 
 function setup() {
@@ -24,8 +27,10 @@ function cursorSide(){
   }
 }
 
+
+
+
 function renderRectangles(){
-  if
   rect(0,0,width/2,height/2); //top left rectangle
   rect(width/2,0,width/2,height/2); //top right rectangle
   rect(0,height/2,width/2,height/2);//bottom left rectangle
@@ -36,5 +41,6 @@ function draw(){
   background(220);
   cursorSide();
   renderRectangles();
+  
 }
  
