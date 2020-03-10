d6aaaa6be4c12f56db702c0cff6549b1abf1b67f
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-03-10 14:10:28 -0600

    triangles

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -9,27 +9,42 @@ let rectWidth = 1;
 let y;
 let yTime = 10;
 let moveRect = 0.1;
+let tallestX,tallest;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
   strokeWeight(0);
+  fill(81, 240, 184);
 
 }
 
-function generateTerrain(){
+function generateTerrain() {
+  strokeWeight(0);
   let yTime = 10;
-  for(let x = 0;x<width;x+=rectWidth){
+  let tallest = height;
+  let tallestX = 0;
+  for (let x = 0; x < width; x += rectWidth) {
     fill(81, 240, 184);
-    rect(x,y,x+rectWidth,height);
-    y = map(noise(yTime),0,1,30,height-100);
-    yTime +=0.005;
+    y = map(noise(yTime), 0, 1, 30, height - 100);
+    rect(x, y, x + rectWidth, height);
+    if (y < tallest) {
+      tallest = y;
+      tallestX = x;
+    }
+    yTime += 0.005;
   }
+  drawFlag(tallestX,tallest);
+  
+}
 
+function drawFlag(x, y) {
+  strokeWeight(1);
+  line(x,y,x,y - 70);
+  triangle(x, y-70, x, y-35, x+35, y-52.5);
 }
 
 function draw() {
   background(220);
   generateTerrain();
-
 }
