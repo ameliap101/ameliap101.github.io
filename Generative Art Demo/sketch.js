36d87a85ee14a373bed5428d219ad1ec4cba1ab3
ameliap101 <60625613+ameliap101@users.noreply.github.com>
2020-03-12 14:10:39 -0600

    colour

--- Generative Art Demo/sketch.js
+++ Generative Art Demo/sketch.js
@@ -1,27 +1,37 @@
 // generative art demo
-let gridSpacing = 20;
-const PADDING = 50;
+const REC_WIDTH = 10;
+const REC_HEIGHT = 50;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+}
 
+function drawRowRGB(yPos) {
+  colorMode(RGB, 255);
+  for (let x = 0; x < width; x += REC_WIDTH) {
+    fill(random(255), random(255), random(255));
+    rect(x, yPos, REC_WIDTH, REC_HEIGHT);
+  }
+}
+
+function drawRowHSB(yPos){
+  colorMode(RGB, 255);
+  for (let x = 0; x < width; x += REC_WIDTH) {
+    fill(x/3%360,map(mouseY,0,height,0,300),300);
+    rect(x, yPos, REC_WIDTH, REC_HEIGHT);
+  }
 }
 
 function border() {
   strokeWeight(15);
-  rect(width/2,height/2, width, height);
+  rect(width / 2, height / 2, width, height);
   strokeWeight(2);
 }
 
-function drawGrid(){
-  for(let x = gridSpacing/2+PADDING;x< width-PADDING;x+= gridSpacing){
-    for(let y = gridSpacing/2+PADDING;y< height-PADDING;y+= gridSpacing){
-      rect(x,y,gridSpacing,gridSpacing);
-    }
-  }
-}
-
 function draw() {
-  border();
-  drawGrid();
+  background(220);
+  randomSeed(0);
+  drawRowRGB(height * 0.2);
+  drawRowHSB(height/2);
 }
